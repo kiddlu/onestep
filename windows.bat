043c1378a6b2d087488e7d873ecf0bd42da5acec
kiddlu <kidd.dawny.lu@gmail.com>
2018-11-14 14:33:35 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- windows.bat
+++ windows.bat
@@ -62,3 +62,13 @@ rem xmlstarlet choco
 rem pt.exe https://github.com/monochromegane/the_platinum_searcher
 rem dll2def ag curl sfk upx ctags make pexports
 rem CDMUninstaller
+
+rem
+rem "python2-32 llvm-32 cmake-64 vs2017
+
+rem .vim\bundle\YouCompleteMe\third_party\install
+rem "C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 15" ..\ycmd\cpp\
+rem "C:\Program Files\CMake\bin\cmake.exe" --build . --target ycm_core --config Release
+
+rem .vimrc
+rem let g:ycm_server_python_interpreter='C:\Python27\python.exe'
