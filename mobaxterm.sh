d2c919dbd8a2640be45c1c2f7f3897b7d406f45a
kiddlu <kidd.dawny.lu@gmail.com>
2021-09-02 14:45:04 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- mobaxterm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~/LauncherFolder/; wget --no-check-certificate https://mobaxterm.mobatek.net/plugins/Git.mxt3
-cd ~/LauncherFolder/; wget --no-check-certificate https://mobaxterm.mobatek.net/plugins/File.mxt3
-cd ~/LauncherFolder/; wget --no-check-certificate https://mobaxterm.mobatek.net/plugins/Curl.mxt3
